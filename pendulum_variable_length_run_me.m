6c6aa9b5e3689aca208337807fca6190dc9f36cc
OliviaYan0613 <cy2395@nyu.edu>
2025-06-28 19:58:26 -0400

    Added t-theta plot

--- pendulum_variable_length_run_me.m
+++ pendulum_variable_length_run_me.m
@@ -144,3 +144,50 @@ end
 close(vidfile);
 
 disp('Video is completed!');
+
+
+
+%% Animation
+%figure;
+%total_length = sum(l);
+%axis_boundary = [-total_length, total_length, -total_length, 0.5];
+%p_handle = plot(0,0,'o-','MarkerFaceColor','b','LineWidth',2); 
+%title('N-Mass Pendulum Animation');
+%xlabel('x (m)');
+%ylabel('y (m)');
+%grid on;
+%axis equal;
+
+%for k = 1:5:length(t)
+    %
+%    X_k = [0; x_t(k, :)'];
+%    Y_k = [0; y_t(k, :)'];
+%    set(plot(0, 0, 'o-', 'MarkerFaceColor', 'b', 'LineWidth', 2), 'XData', X_k, 'YData', Y_k);
+%    axis(axis_boundary);
+%    pause(0.01);
+%end
+
+% Plot
+%figure;
+%plot(t, theta_t, 'LineWidth', 1.5);
+%title('Angle-Time Plot');
+%xlabel('Time (s)');
+%ylabel('Angle (radians)');
+%grid on;
+%legend_str = cell(n, 1);
+%for i = 1:n
+%   legend_str{i} = sprintf('Theta %d', i);
+%end
+%legend(legend_str, 'Location', 'northeast');
+
+%% Plot
+figure;
+hold on;
+for i = 1:n
+    plot(t, Y(:,i), 'LineWidth', 1.5,'color', colors(i,:));
+    title('Angle-Time Plot');
+    xlabel('Time (s)');
+    ylabel('Angle (radians)');
+    grid on;
+end
+%legend(legend_str, 'Location', 'northeast');
