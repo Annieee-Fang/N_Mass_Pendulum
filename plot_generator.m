f42ed9d942d00d8bf338e582b3ceeefaec7f81ca
OliviaYan0613 <cy2395@nyu.edu>
2025-07-01 05:13:44 -0400

    Added plot generator to compare the t-theta plots of the last mass for different changing period

---
+++ plot_generator.m
@@ -0,0 +1,137 @@
+clear; clc; close all;
+
+% parameters
+n = 5;
+g = 9.81; 
+m_0 = 1;
+l_0 = 2;
+l = ones(n, 1) * l_0;
+l_max = 2; % range of length
+l_min = 1;
+nf = sqrt(g/l_0); % natural frequency of pendulums
+conc = 0; % concavity of change of length
+
+%% Masses
+mass_case = 'same';'descent';
+switch mass_case
+    case 'same'
+        m = ones(n, 1) * m_0;
+    case 'descent'
+        m = ones(n, 1);
+        for i = 1:n
+            m(i) = 0.1^(i-1) * m_0;
+        end
+end
+
+%% Initial Conditions
+extForce_case = 'noExtForce';'extForce';
+switch extForce_case
+    case 'noExtForce'
+        theta_initial = ones(n, 1) * (pi/10); % Initial angles
+        omega_initial = zeros(n, 1); % Initial angular velocities
+        C = 0; % no external force
+        omega_0 = 0;
+    case 'extForce'
+        theta_initial = zeros(n,1); % zero initial conditions
+        omega_initial = zeros(n,1);
+        C = .5; % amplitude of external force
+
+        % driving frequency of external force
+        omega_0 = nf+0.1;  % near natural frq
+        %omega_0 = 2*nf; % far from natural frq
+end
+
+Theta_initial = [theta_initial; omega_initial];
+
+%% ODE Solver
+t_start = 0;
+t_end = 15;
+t_span = [t_start t_end]; 
+
+ct0 = 0;
+ct_step = 15;
+ct_span = [0,ct0]; % time span of length changing period
+
+for k = 1:ct_step
+    ct = ct0 + k*(t_end-ct0)/ct_step;
+    ct_span = [0,ct];
+
+    [t, Y] = ode45(@(t,Theta) pendulum_ct(t, Theta, n, g, m, l, C, omega_0, l_max, l_min, conc, ct_span), t_span, Theta_initial);
+    for i = 1:length(t)
+        t_size(k) = length(t);
+        Yn(i,k) = Y(i,n);
+    end
+end
+
+% only keep the data before shortest t
+Y_mod = zeros(min(t_size), ct_step);
+for i = 1:min(t_size)
+    t_mod(i) = t(i);
+    Y_mod(i,:) = Yn(i,:);
+end
+
+N = ct_step;
+
+% plotting
+fig = figure;
+cmap = colormap(parula(N+1));
+colors = cmap; 
+
+%% Plot
+figure;
+hold on;
+for k = 1:ct_step
+    plot(t_mod, Y_mod(:,k), 'LineWidth', 1.5,'color', colors(k,:));
+    title('Angle-Time Plot');
+    xlabel('Time (s)');
+    ylabel('Angle (radians)');
+    grid on;
+end
+
+
+%% Set up the VideoWriter object
+%vidfile = VideoWriter('N-link_pendulum_animation.mp4', 'MPEG-4');
+%vidfile.FrameRate = 25;   % Slower playback if the number is smaller (was 25)
+%vidfile.Quality = 100;
+%open(vidfile);
+
+%set(gcf,'position',[476 360 400 300])
+%set(gcf,'color','w')
+%for i = 1:2:length(t)
+%    clf;
+%    hold on;
+
+%    for j = N:-1:1
+%        color_at_time = colors(j, :);  % fixed color for each mass
+%        h = plot(x_trajectory(1:i,j+1), y_trajectory(1:i,j+1), 'Color', color_at_time, 'LineWidth', 1.5);
+%        alpha_val = .1; % transparency for whole trajectory
+%        set(h, 'Color', [color_at_time, alpha_val]);  % apply transparency to the color
+%    end
+
+%    plot(0, 0, 'ko', 'MarkerSize', 10, 'MarkerFaceColor', 'k'); % pivot point at (0, 0)
+%    plot(x(i,1), y(i,1), 'o', 'MarkerSize', 10, 'MarkerFaceColor', colors(1,:),...
+%        'Color', colors(j,:),'MarkerEdgeColor','k');
+%    plot([0 x(i,1)],[0 y(i,1)], '-','Color', colors(1,:), 'LineWidth', 2)
+%    for j = 2:N
+%        plot([x(i,j-1), x(i,j)], [y(i,j-1), y(i,j)], '-','Color', colors(j,:), 'LineWidth', 2);
+%        plot(x(i,j), y(i,j), 'o', 'MarkerSize', 10, 'MarkerFaceColor', colors(j,:),...
+%            'Color', colors(j,:),'MarkerEdgeColor','k')
+%    end
+
+ %   axis equal;
+ %   box on
+ %   set(gca,'fontsize',16,'ticklabelinterpreter','latex')
+ %   title(sprintf('%d-link pendulum with strings, t = %.2f s', N, t(i)),'interpreter','latex','FontSize',20);
+ %   axis([-N N -N-2 .5]);
+ %   xticks(-6:2:6)
+  %  yticks(-6:2:0)
+    % drawnow;
+    % Capture the frame and write to video
+ %   F = getframe(fig);
+ %   writeVideo(vidfile, F);
+%end
+
+% Close the video file after the loop
+%close(vidfile);
+
+%disp('Video is completed!');
